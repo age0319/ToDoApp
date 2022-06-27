65eb0ae546339f4fb6b55ea04a57ac8326293a05
nokkun <at.mc1.18@gmail.com>
2022-06-27 15:59:33 +0900

    時刻とチェックボックスの追加。

--- app/src/main/java/com/atmc118/todoapp/MainActivity.kt
+++ app/src/main/java/com/atmc118/todoapp/MainActivity.kt
@@ -11,9 +11,14 @@ import androidx.compose.runtime.*
 import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.atmc118.todoapp.ui.theme.ToDoAppTheme
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,9 +30,12 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-//                    Greeting("Android")
-                    val todoList = (1..100).map { "タスク ${it}" }
-                    MainScreen()
+                    var todoList: SnapshotStateList<Task> by remember { mutableStateOf(mutableStateListOf()) }
+                    val task = Task()
+                    task.task = "買い物に行く"
+                    task.time = Date()
+                    todoList.add(task)
+                    MainScreen(todoList)
                 }
             }
         }
@@ -35,39 +43,73 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun MainScreen() {
+fun MainScreen(todoList: SnapshotStateList<Task>) {
 
-    var todoList: SnapshotStateList<String> by remember { mutableStateOf(mutableStateListOf()) }
     var text: String by remember { mutableStateOf("") }
+    val sdf = SimpleDateFormat("yyyy/MM/dd HH:mm:ss")
 
     Column {
         TopAppBar(
             title = { Text("Todo List") }
         )
         LazyColumn(
-            modifier = Modifier.fillMaxWidth().weight(1f)
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1f)
         ) {
             items(todoList) { todo ->
-                Text(
-                    text = todo,
-                    modifier = Modifier.fillMaxWidth().padding(16.dp)
-                )
+                var checkedState:Boolean by remember { mutableStateOf(false) }
+                Row( modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)) {
+                    Checkbox(
+                        checked = checkedState,
+                        onCheckedChange = {
+                            checkedState = it
+                        })
+                    if(checkedState) {
+                        Text(
+                            text = todo.task,
+                            style = TextStyle(textDecoration = TextDecoration.LineThrough),
+                            modifier = Modifier
+                                .padding(16.dp)
+                        )
+                    }else{
+                        Text(
+                            text = todo.task,
+                            modifier = Modifier
+                                .padding(16.dp))
+                    }
+                    Text(
+                        text = sdf.format(todo.time),
+                        color = Color.LightGray,
+                        modifier = Modifier
+                            .padding(16.dp)
+                    )
+                }
             }
         }
         Row(
-            modifier = Modifier.fillMaxWidth().padding(16.dp)
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
         ) {
             OutlinedTextField(
                 value = text,
                 onValueChange = {text = it},
                 label = { Text("ToDo") },
-                modifier = Modifier.wrapContentHeight().weight(1f)
+                modifier = Modifier
+                    .wrapContentHeight()
+                    .weight(1f)
             )
             Spacer(Modifier.size(16.dp))
             Button(
                 onClick = {
                     if (text.isEmpty()) return@Button
-                    todoList.add(text)
+                    val task = Task()
+                    task.task = text
+                    task.time = Date()
+                    todoList.add(task)
                     text = ""
                 },
                 modifier = Modifier.align(Alignment.CenterVertically)
@@ -78,15 +120,19 @@ fun MainScreen() {
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     ToDoAppTheme {
-        Greeting("Android")
+        var todoList: SnapshotStateList<Task> by remember { mutableStateOf(mutableStateListOf()) }
+        var task = Task()
+        task.task = "買い物に行く"
+        task.time = Date()
+        todoList.add(task)
+        task = Task()
+        task.task = "掃除をする"
+        task.time = Date()
+        todoList.add(task)
+        MainScreen(todoList)
     }
 }
---
+++ app/src/main/java/com/atmc118/todoapp/Task.kt
@@ -0,0 +1,9 @@
+package com.atmc118.todoapp
+
+import java.util.*
+
+class Task {
+    var task:String = ""
+    var time:Date = Date()
+    var isDone:Boolean = false
+}
